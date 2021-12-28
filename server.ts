75f330cf24f2762c478f6b4a38f6c412638485ad
profMana <roberto.mana@vallauri.edu>
2021-12-28 18:38:39 +0100

    starting version

---
+++ server.ts
@@ -0,0 +1,272 @@
+import * as http from "http";
+import * as fs from "fs";
+import * as bodyParser from "body-parser";
+import * as mongodb from "mongodb";
+import express from "express";
+const app=express();
+import cors from "cors";
+
+// mongo
+const mongo = require("mongodb");
+const mongoClient = mongo.MongoClient;
+const ObjectId = mongo.ObjectId;
+// const CONNECTIONSTRING =  "mongodb://127.0.0.1:27017";
+const CONNECTIONSTRING =  "mongodb+srv://admin:admin@cluster0.xoopk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTIONOPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+const dbNAME = "recipeBook"
+const PORT = process.env.PORT || 1337
+
+/* compass:
+ connection string new version:
+    mongodb+srv://admin:admin@cluster0.xoopk.mongodb.net/test
+
+ connection string old version:    
+    mongodb://admin:admin@cluster0-shard-00-00.xoopk.mongodb.net:27017,cluster0-shard-00-01.xoopk.mongodb.net:27017,cluster0-shard-00-02.xoopk.mongodb.net:27017/test?replicaSet=atlas-31rhvw-shard-0&ssl=true&authSource=admin
+*/
+
+
+/* ***************************** Avvio Server ****************************** */
+
+const server = http.createServer(app)
+server.listen(PORT, function() {
+    console.log("Server in ascolto sulla porta " + PORT);
+    init();
+});
+
+let paginaErrore = "";
+function init() {
+    fs.readFile("./static/error.html", function(err, data) {
+        if (!err)
+            paginaErrore = data.toString();
+        else
+            paginaErrore = "<h1>Risorsa non trovata</h1>"
+    });	
+}
+
+
+
+/* ************************** SEZIONE 1 : Middleware ********************** */
+
+// 1. Request log
+app.use("/", function (req,res,next) {
+    console.log(req.method + " : " + req.originalUrl);
+    next();
+});
+
+// 3 - route risorse statiche
+app.use("/", express.static('./static'));
+
+// 4 - routes di lettura dei parametri post
+app.use("/", bodyParser.json());   
+app.use("/", bodyParser.urlencoded({extended: true })); 
+
+// 6 - log dei parametri 
+app.use("/", function (req, res, next) {
+	// if(req.query != {}) NOK perchè i puntatori sono diversi
+    if(Object.keys(req.query).length != 0)
+	    console.log("------> Parametri GET: " + JSON.stringify(req.query));
+	if(Object.keys(req.body).length != 0)
+	    console.log("------> Parametri BODY: " +JSON.stringify(req.body));
+    next();
+});
+
+
+// 7 - CORS Policy
+const whitelist = ["http://localhost:1337", "http://localhost:4200"];
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (!origin)
+            return callback(null, true);
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        } 
+	  else
+            return callback(null, true);
+    },
+    credentials: true
+};
+app.use("/", cors(corsOptions));
+
+
+
+
+
+
+/* ********************** (SEZIONE 2) CLIENT REQUEST  *********************** */
+
+// apertura della connessione
+app.use("/api/", function (req, res, next) {
+    mongoClient.connect(CONNECTIONSTRING, function(err, client) {
+        if (err) {
+            let msg = "Errore di connessione al db"
+            res.status(503).send(msg)
+        } 
+		else {
+			req["client"]=client;
+			next();
+		}
+	})	
+})
+ 
+ 
+// elenco collezioni
+app.get('/api/getCollections', function(req, res, next) {
+	let db = req["client"].db(dbNAME);
+	db.listCollections().toArray(function(err, collections) {
+		if (err) {
+			res.status(500).send("Errore lettura elenco collezioni")
+		} 
+		else {
+			res.send(collections)
+		}
+		req["client"].close();
+	})
+}); 
+ 
+
+// Lettura collection e id (per qualsiasi metodo: get, post, put, etc)
+let currentCollection
+let currentId;
+app.use("/api/:collection/:id?", function(req, res, next) {
+    currentCollection = req.params.collection
+    currentId = req.params.id  // se non c'è sarà undefined
+    next()
+})
+
+
+
+
+
+
+
+
+
+/* ************************************************************************** */
+
+/* GET REQUEST
+   /api/ davanti all'ASTERISCO NON può essere tolto perchè altrimenti, 
+   se l'utente richiede una risorsa statica che non viene trovata, 
+   poi il runtime esegue questa route e va in errore                */   
+
+app.get('/api/*', function(req, res, next) {
+	let db = req["client"].db(dbNAME);
+	let collection = db.collection(currentCollection);
+	// Elenco
+	if (!currentId) {
+		collection.find(req.query).toArray(function(err, data) {
+			if (err) {
+				res.status(500).send("Errore esecuzione query")
+			} 
+			else {
+				res.send(data)
+			}
+			req["client"].close();
+		})
+	} 
+
+	// dettagli
+	else {
+		let oid = new ObjectId(currentId); 
+		collection.findOne({ "_id": oid }, function(err, data) {
+			if (err) {
+				res.status(500).send("Errore esecuzione query")
+			} else {
+				res.send(data)
+			}
+			req["client"].close();
+		})
+	}
+});
+
+
+app.post('/api/*', function(req, res, next) {
+	let record = req.body
+	let db = req["client"].db(dbNAME);
+	let collection = db.collection(currentCollection);
+	collection.insertOne(record, function(err, data) {
+		if (err) {
+			res.status(500).send("Errore esecuzione query")
+		} else {
+			res.send(data)
+		}
+		req["client"].close();
+	})
+});
+
+
+app.delete('/api/*', function(req, res, next) {
+	let db = req["client"].db(dbNAME);
+	let collection = db.collection(currentCollection);
+	let oid = new ObjectId(currentId);
+	collection.deleteOne({ "_id": oid }, function(err, data) {
+		if (err) {
+			res.status(500).send("Errore esecuzione query")
+		} else {
+			res.send(data)
+		}
+		req["client"].close();
+	})
+});
+
+
+app.patch('/api/*', function(req, res, next) {
+	let record = req.body
+	let db = req["client"].db(dbNAME);
+	let collection = db.collection(currentCollection);
+	let oid = new ObjectId(currentId);
+	collection.updateOne({ "_id": oid }, { "$set": record },
+		function(err, data) {
+			if (err) {
+				res.status(500).send("Errore esecuzione query")
+			} else {
+				res.send(data)
+			}
+			req["client"].close();
+		}
+	)
+});
+
+app.put('/api/*', function(req, res, next) {
+	let record = req.body
+	let db = req["client"].db(dbNAME);
+	let collection = db.collection(currentCollection);
+	console.log(currentCollection)
+	console.log(currentId)
+	console.log(record)
+	let oid = new ObjectId(currentId);
+	collection.replaceOne({ "_id": oid }, record, function(err, data) {		
+		if (err) {
+			console.log(err)
+			res.status(500).send("Errore esecuzione query")
+		} else {
+			res.send(data)
+		}
+		req["client"].close();
+	}) 
+});
+
+
+
+
+
+/* ********************** (Sezione 3) DEFAULT ROUTE  ************************ */
+
+// 2 - default route
+app.use('/', function (req, res, next) {
+    res.status(404)
+    if (req.originalUrl.startsWith("/api/")) {
+        // se status != 200 mando una semplice stringa
+        res.send("Risorsa non trovata");
+		req["client"].close();
+    }
+    else  
+		res.send(paginaErrore);
+});
+
+
+// gestione degli errori
+app.use(function(err, req, res, next) {	
+	 console.log("***** SERVER ERROR ****** : ",  err.message);  
+});
---
+++ static/index.js
@@ -0,0 +1,164 @@
+"use strict"
+$(document).ready(function() {
+    let divIntestazione = $("#divIntestazione")
+    let divFilters = $(".card").eq(0)
+    let divCollections = $("#divCollections")
+    let table = $("#mainTable")
+    let divDettagli = $("#divDettagli")
+    let currentCollection = "";
+
+    divFilters.hide()
+	$("#lstHair").prop("selectedIndex", -1);
+    
+	let request = inviaRichiesta("get", "api/getCollections");
+    request.fail(errore)
+    request.done(function(collections) {
+        let label = divCollections.children("label")
+        for (let collection of collections) {
+            let clone = label.clone().appendTo(divCollections)
+            clone.children("span").text(collection.name)
+            clone.children("input").val(collection.name)
+			divCollections.append("<br>")
+        }
+        label.remove()
+    })
+
+
+    divCollections.on("click", "input[type=radio]", function() {
+		currentCollection = $(this).val()
+		if(currentCollection=="unicorns")
+		    divFilters.show()
+		else
+			divFilters.hide()
+		richiediElenco()
+    })
+
+	
+	// Questa procedura sarà riutilizzata più volte
+	function richiediElenco(){
+        let request = inviaRichiesta("get", "/api/" + currentCollection)
+        request.fail(errore)
+        request.done(aggiornaTabella)		
+	}
+
+	
+	// Anche quest'altra procedura sarà riutilizzata più volte
+    function aggiornaTabella(data) {
+        divIntestazione.find("strong").first().html(currentCollection)
+        divIntestazione.find("strong").last().html(data.length)
+        table.children("tbody").empty();
+        if (data.length != 0) {
+            for (let item of data) {
+                let tr = $("<tr>").appendTo(table.children("tbody"))
+                $("<td>").appendTo(tr).text(item._id).prop("cod",item["_id"])
+					            .prop("method","get").on("click", details)
+                $("<td>").appendTo(tr).text(item.name).prop("cod",item["_id"])
+					            .prop("method","get").on("click", details)
+                // pulsantini
+				let td = $("<td>").appendTo(tr)				
+				$("<div>").appendTo(td).prop("cod", item["_id"])
+				        .prop("method","patch").on("click", details)
+				$("<div>").appendTo(td).prop("cod", item["_id"])
+				          .prop("method","put").on("click", details)
+				$("<div>").appendTo(td).prop("cod", item["_id"])
+				          .on("click", elimina)
+			}
+		}
+		divDettagli.empty();
+    }
+
+
+
+    function details() {
+		let method = $(this).prop("method")
+		let cod = $(this).prop("cod")		
+        let resource = "/api/" + currentCollection + "/" + cod
+        let request = inviaRichiesta("get", resource)
+        request.fail(errore)
+		request.done(function(data) {
+			if (method=="get"){
+				let content = ""
+				for (let key in data)  
+					content+= "<strong>"+key+"</strong> : " + data[key] + "<br>"
+				divDettagli.html(content)
+			}
+			else{  // ******* DOPO *******
+				// ID non è modificabil quindi NON deve essere inviato al server
+				delete(data["_id"])
+				divDettagli.empty();
+				let textArea = $("<textArea>").appendTo(divDettagli)
+				textArea.val(JSON.stringify(data, null, 2))
+				// Imposto altezza textArea al valore del contenuto
+				textArea.css("height", textArea.get(0).scrollHeight + "px")
+				aggiungiButtonInvia(method, cod)
+			}	
+        })
+    }
+	
+
+	$("#btnAdd").on("click", function(){
+		divDettagli.empty();
+		let textArea = $("<textArea>").val("{  }").appendTo(divDettagli)
+		// In un primo tempo scrivere il codice di buttonInvia qui IN LOCO
+		aggiungiButtonInvia("post", "")			
+	})
+
+
+    function elimina() {
+        let cod = $(this).prop("cod")
+        if (confirm("vuoi veramente cancellare il record " + cod + "?")) {
+            let request = inviaRichiesta("delete", "/api/" + currentCollection
+                                                           + "/" + cod)
+            request.fail(errore)
+            request.done(function(data) {
+                alert("Record rimosso correttamente")
+                richiediElenco()
+            })
+        }
+    }
+
+	
+	function aggiungiButtonInvia(method, cod="") {
+        $("<button>").text("invia").appendTo(divDettagli).on("click",function(){
+            let param;
+            try {
+                param = JSON.parse($(this).prev().val())
+            } 
+			catch { 
+				alert("JSON non valido"); 
+				return;  
+			}			
+            let request = inviaRichiesta(method, "/api/" + currentCollection 
+			                                             + "/" + cod, param)
+            request.fail(errore)
+            request.done(function(data) {
+                alert("Operazione eseguita correttamente")
+				richiediElenco()
+            })
+        })
+    }
+	
+	
+	
+	$("#btnFind").on("click", function(){
+		let filter = {}
+		let hair = $("#lstHair").children("option:selected").val()
+		if (hair)
+			filter["hair"]=hair.toLowerCase();
+		
+		let male = divFilters.find("input[type=checkbox]").first()
+				.is(":checked")
+		let female = divFilters.find("input[type=checkbox]").last()
+				.is(":checked")
+		if(male && !female)
+			filter["gender"]='m';
+		else if(female && !male)
+			filter["gender"]='f';
+		
+		let request = inviaRichiesta("get", "/api/" + currentCollection, filter)
+		request.fail(errore)
+		request.done(function(data){
+			aggiornaTabella(data)			
+		})
+	})
+});
